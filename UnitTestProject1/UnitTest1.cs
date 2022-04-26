e55496d37be6f14b899384276c3239e00feeca1b
Даниил <kourin.daniil@gmail.com>
2022-04-26 09:07:29 +0300

    <Required>

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using ClassLib;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
@@ -9,7 +11,10 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
+            List<Task> task = new List<Task>();
+            TaskList taskList = new TaskList("Входящие", task);
 
+            Assert.AreEqual(taskList.GetName(), "Входящие");
         }
     }
 }
